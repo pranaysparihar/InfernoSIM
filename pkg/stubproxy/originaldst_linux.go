5041641ebd3ef13e366c2955f2111428c9604955
Pranay Singh Parihar <pranaysparihar@Pranays-MacBook-Pro.local>
2026-03-16 18:09:29 +0530

    fix: linux/386 build by using unix package for getsockopt

--- pkg/stubproxy/originaldst_linux.go
+++ pkg/stubproxy/originaldst_linux.go
@@ -3,9 +3,12 @@
 package stubproxy
 
 import (
+	"fmt"
 	"net"
 	"syscall"
 	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
 const soOriginalDst = 80
@@ -15,14 +18,16 @@ func originalDst(conn *net.TCPConn) (string, int, error) {
 	if err != nil {
 		return "", 0, err
 	}
+
 	var addr syscall.RawSockaddrInet4
 	var opErr error
-	raw.Control(func(fd uintptr) {
+
+	err = raw.Control(func(fd uintptr) {
 		l := uint32(unsafe.Sizeof(addr))
-		_, _, errno := syscall.Syscall6(
-			syscall.SYS_GETSOCKOPT,
+		_, _, errno := unix.Syscall6(
+			unix.SYS_GETSOCKOPT,
 			fd,
-			uintptr(syscall.SOL_IP),
+			uintptr(unix.SOL_IP),
 			uintptr(soOriginalDst),
 			uintptr(unsafe.Pointer(&addr)),
 			uintptr(unsafe.Pointer(&l)),
@@ -32,10 +37,15 @@ func originalDst(conn *net.TCPConn) (string, int, error) {
 			opErr = errno
 		}
 	})
+
+	if err != nil {
+		return "", 0, err
+	}
 	if opErr != nil {
 		return "", 0, opErr
 	}
-	ip := net.IPv4(addr.Addr[0], addr.Addr[1], addr.Addr[2], addr.Addr[3]).String()
+
+	ip := fmt.Sprintf("%d.%d.%d.%d", addr.Addr[0], addr.Addr[1], addr.Addr[2], addr.Addr[3])
 	port := int(addr.Port>>8) | int(addr.Port&0xff)<<8
 	return ip, port, nil
 }
